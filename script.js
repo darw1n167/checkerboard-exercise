004e8758b58ae4163496126666b0d56838efac4d
Darwin Facelo <darwinfacelo@yahoo.com>
2022-12-14 15:31:35 -0800

    started making squares

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Your JS goes here
+const body = document.body.classList.add('container')
+
+function makeSquares ()  {
+    const div = document.createElement('div');
+    const body = document.getElementsByTagName('body');
+    div.className = 'square';
+    div.style.backgroundColor = 'red';
+    div.style.border = 'black solid 1px';
+    div.style.width = '100px';
+    div.style.height = '100px';
+    document.body.appendChild(div);
+}
+
+function createRow (num)  {
+    for (let i = 0; i < num; i++)  {
+        makeSquares();
+    }
+}
+
+createRow(50);
