694449b42fba8adadef4fe6e8cc052f1d55b623e
Darwin Facelo <darwinfacelo@yahoo.com>
2022-12-14 15:58:52 -0800

    looped makesquares 100 times by putting it inside createBoard function

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ function makeSquares ()  {
     const div = document.createElement('div');
     const body = document.getElementsByTagName('body');
     div.className = 'square';
-    div.style.backgroundColor = 'red';
-    div.style.border = 'black solid 1px';
+    div.style.backgroundColor = 'black';
     div.style.width = '11.1%';
     div.style.height = '11.1%';
     div.style.paddingBottom = '11.1%'
@@ -15,14 +14,14 @@ function makeSquares ()  {
     return div;
 }
 
-function createColumn (num)  {
-    for (let i = 0; i < num; i++)  {
+function createBoard (num)  {
+    for (let i = 1; i < num; i++)  {
         if (i % 2 === 0) {
         makeSquares();
         } else {
-            makeSquares().style.backgroundColor = 'black';
+            makeSquares().style.backgroundColor = 'red';
         }
     }
 }
 console.log(makeSquares())
-createColumn(55);
+createBoard(100)
